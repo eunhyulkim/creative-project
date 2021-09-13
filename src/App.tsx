f3d87199f90d0c0ea61feb67c8c6d617339a7d97
김은휼 <gim-eunhyul@gim-eunhyul-ui-MacBookPro.local>
2021-09-13 20:18:54 +0900

    FIX: default index 파일 수정을 이용해 thumnail 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { WritingApp, HomeApp } from './apps/type';
-import SEO from './SEO';
 
 function App(): JSX.Element {
 	return (
 		<>
-			<SEO />
 			<Router>
 				<Switch>
 					<Route path="/writer" component={WritingApp} />
