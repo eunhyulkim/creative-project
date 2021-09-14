890090e5b5b854f597b1196612786db86ce22eae
김은휼 <valhalla.host@gmail.com>
2021-09-15 02:19:35 +0900

    FEAT: 북커버 SVG 8종 컴포넌트로 제작 일러스트레이터로 작업, 텍스트 위치 고려하여 도형 위치 조정 필요 Create-Book-App

---
+++ src/assets/book_cover/index.tsx
@@ -0,0 +1,8 @@
+export { ReactComponent as CoverZero } from './0.svg';
+export { ReactComponent as CoverOne } from './1.svg';
+export { ReactComponent as CoverTwo } from './2.svg';
+export { ReactComponent as CoverThree } from './3.svg';
+export { ReactComponent as CoverFour } from './4.svg';
+export { ReactComponent as CoverFive } from './5.svg';
+export { ReactComponent as CoverSix } from './6.svg';
+export { ReactComponent as CoverSeven } from './7.svg';
